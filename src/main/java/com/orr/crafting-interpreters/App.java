aa7f2420296cb3337e9dab665fde2bb581fddcc9
Orr Shalev <shalev.orr@gmail.com>
2024-05-04 19:11:19 -0400

    maven start

---
+++ src/main/java/com/orr/crafting-interpreters/App.java
@@ -0,0 +1,13 @@
+package com.orr.crafting-interpreters;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
